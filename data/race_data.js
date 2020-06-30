48755c140f97720e0b483fef211b0a07867cf08a
Harlen <dolldream2012@gmail.com>
2020-06-30 17:30:50 +0800

    建立角色 - 性別、名字

--- data/race_data.js
+++ data/race_data.js
@@ -1,25 +1,37 @@
 /*
-    編號規則:
-    千位數為屬性
-    十位數為種族
-    個位數為亞種
+編號規則:
+	千位數為屬性
+	十位數為種族
+	個位數為亞種
 */
 
+// 模版
+const model = {
+	name: "",	// 種族名稱
+	gender: [1, 1, 49, 49],	// 種族性別比例(0: 多性, 1:無性, 2:女性, 3:男性)
+	rate: 0,	// 種族權重(越高越容易影響特徵)
+};
+
 module.exports = {
-    // 0 ~ 999 古族
-    0000: { name: "古族" },
-    0990: { name: "威爾德" },
-    // 1000 ~ 1999 風神眷族
-    // 2000 ~ 2999 水神眷族
-    // 3000 ~ 3999 火神眷族
-    3000: { name: "精靈" },
-    3001: { name: "白精靈" },
-    3002: { name: "黑精靈" },
-    // 4000 ~ 4999 地神眷族
-    // 5000 ~ 5999 獸神眷族
-    5990: { name: "人類" },
-    5991: { name: "真人" },
-    5992: { name: "異人" },
-    5993: { name: "耶畢特人" },
-    // 6000 ~ 6999 魔物
+	// 0 ~ 999 古族
+	0000: Object.assign({}, model, { name: "古族", gender: [0, 100, 0, 0] }),
+	0990: Object.assign({}, model, { name: "威爾德", rate: 20, gender: [10, 10, 40, 40] }),
+	// 1000 ~ 1999 風神眷族
+	// 2000 ~ 2999 水神眷族
+	// 3000 ~ 3999 火神眷族
+	3000: Object.assign({}, model, { name: "精靈", rate: 100 }),
+	3001: Object.assign({}, model, { name: "原生精靈", rate: 100 }),
+	3001: Object.assign({}, model, { name: "白精靈", rate: 100 }),
+	3002: Object.assign({}, model, { name: "黑精靈", rate: 100 }),
+	// 4000 ~ 4999 地神眷族
+	// 5000 ~ 5999 獸神眷族
+	5990: Object.assign({}, model, { name: "人類", rate:50 }),
+	5991: Object.assign({}, model, { name: "真人", rate:50 }),
+	5992: Object.assign({}, model, { name: "異人", rate:50 }),
+	5993: Object.assign({}, model, { name: "耶畢特人", rate:50 }),
+	// 6000 ~ 6999 魔物
+	6800: Object.assign({}, model, { name: "不死系魔物"}),
+	// 測試種族
+	998: Object.assign({}, model, { name: "測試種族1", rate:100, gender: [1, 1, 49, 49]}),
+	999: Object.assign({}, model, { name: "測試種族2", rate:100, gender: [1, 1, 80, 20]}),
 }
--- data/skill_data.js
+++ data/skill_data.js
@@ -1,17 +1,25 @@
 /*
-name: 技能名稱
-max_lv: 技能等級上限
-up_list: 上位技能
-race: 可學習種族
-race_f: 無法學習種族
-flag: 獲得條件
-※判斷可否學習邏輯: 先挑選可學習種族，再剔除無法學習種族
+判斷可否學習邏輯: 先挑選可學習種族，再剔除無法學習種族
 */
+
+// 模版
+const model = {
+	name: "",	// 技能名稱
+	type: "",	// 類型(0:被動, 1:攻擊, 2:輔助)
+	max_lv: 1,	// 技能等級上限
+	up_list: [],	// 上位技能
+	race: [],	// 可學習種族
+	race_f: [],	// 無法學習種族
+	flag: {},	// 獲得條件
+	cost: {},	// 消耗
+	effect: {},	// 效果
+}
+
 module.exports = {
 	// 再生: 每回合恢復HP 10 ~ 100
-	0: { name: "再生", max_lv: 10, up_list: [1, 2], race: [], race_f: [], flag: {} },
+	0: Object.assign({}, model, { name: "再生", max_lv: 10, up_list: [1, 2] }),
 	// 高速再生: 每回合恢復HP 6% ~ 15%／再生(0) lv6、HP > 1000
-	1: { name: "高速再生", max_lv: 10, up_list: [2], race: [], race_f: [], flag: { "skill_0":">5", "hp":">1000"} },
-	// 超速再生: 每回合恢復HP 16% ~ 20% 或恢復HP 至 20 ~ 50%／高速再生(1) lv6／限定不死系魔物、鳳凰可學習
-	2: { name: "超速再生", max_lv: 5, up_list: [], race: [], race_f: [] },
+	1: Object.assign({}, model, { name: "高速再生", max_lv: 10, up_list: [2], flag: { "skill_0": ">5", "hp": ">1000" } }),
+	// 超速再生: 每回合恢復HP 16% ~ 20% 或恢復HP 至 20 ~ 50%／高速再生(1) lv6／限定不死系魔物可學習
+	2: Object.assign({}, model, { name: "超速再生", max_lv: 5, race: [6800], flag: { "skill_1": ">5" } }),
 }
